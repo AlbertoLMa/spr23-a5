a477a0cafde01fe8e52d09cdd0bf430fe4c9776a
alberto <alberto9@stanford.edu>
2023-05-17 16:00:48 -0700

    added make functionality

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -4,7 +4,13 @@ from calculator_adapter import run
 
 ### ADD AT LEAST TWO TESTS HERE!
 
+# Checks that the program outputs "0" for an input of "1 * 0"
+assert run("1 * 0").output == "0"
 
-###
+# Checks that the program exits successfully for input "5 * 0"
+assert run("5 * 0").output == "0" 
 
 print("All tests passed!")
+
+
+
